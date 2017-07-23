1b3c5708fb29d16a3e2bcfe0f418baa25b57fc7f
gozart1 <gozart1@users.noreply.github.com>
2017-07-23 14:41:06 -0700

    transactions chart

---
+++ src/components/Transactions.vue
@@ -0,0 +1,160 @@
+<template>
+  <div  class="container-fluid">
+
+    <div class="row">
+      <div class="col-12">
+        <h3>Transactions</h3>
+      </div>
+    </div>
+
+    <div v-if="blockSizeRange">
+      <line-chart
+        :height="300"
+        :chart-data="{
+          labels: blockSizeRangeLabels,
+          datasets:[
+            {
+              label: 'Block Size',
+              yAxisID: 'y-axis-0',
+              fill: false,
+              backgroundColor: '#2970ff',
+              borderColor: '#2970ff',
+              data: blockSizeRange
+            }
+          ]
+        }"
+        :options="blockSizeRangeOptions"
+      ></line-chart>
+      <div class="text-center" style="margin-top: -5px;"><small>Block Height</small></div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+import helpers from '../helpers'
+import LineChart from '@/components/LineChart.js'
+import axios from 'axios'
+import _ from 'lodash'
+import log from 'loglevel'
+
+function getTransactions (start, end, data) {
+  let url = helpers.apiUrl + 'block/range/' + start + '/' + end
+  axios.get(url).then(function (response) {
+    var compactedResponseData = _.compact(response.data)
+    log.info('compactedResponseData', compactedResponseData)
+
+    let chartData = {
+      blockSizeRange: _.map(compactedResponseData, 'size'),
+      blockSizeRangeLabels: _.map(compactedResponseData, (v) => {
+        return v.height.toLocaleString()
+      }),
+      blockSizeRangeOptions: {
+        animation: {
+          duration: 100
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+        tooltips: {
+          enabled: true,
+          mode: 'single',
+          callbacks: {
+            title: function (tooltipItems, data) {
+              return 'Block Height: ' + tooltipItems[0].xLabel
+            },
+            label: function (tooltipItem, data) {
+              return helpers.formatBytes(tooltipItem.yLabel)
+            }
+          }
+        },
+        scales: {
+          yAxes: [
+            {
+              position: 'left',
+              id: 'y-axis-0',
+              ticks: {
+                beginAtZero: false,
+                callback: function (label, index, labels) {
+                  return helpers.formatBytes(label)
+                    // return label.toLocaleString()
+                }
+              },
+              scaleLabel: {
+                labelString: 'Block Size',
+                display: true
+              }
+            }
+          ]
+        }
+      }
+    }
+    _.assign(data, chartData)
+  })
+  .catch(function (error) {
+    log.info('fetchBlockRange error', error)
+  })
+}
+
+export default {
+  data () {
+    return {
+      loading: false,
+      start: null,
+      end: null,
+      bestBlock: null,
+      avgTimeBlockTimeInMinutes: null,
+      lastBlockTimeElapsed: null,
+      blockSizeRange: null,
+      blockSizeRangeLabels: null,
+      blockSizeRangeOptions: null,
+      stakeDiff: null,
+      ticketPrices: null,
+      error: null
+    }
+  },
+  components: {
+    LineChart
+  },
+  created () {
+    // fetch the data when the view is created and the data is
+    // already being observed
+    var _this = this
+    axios.get(helpers.apiUrl + 'block/best')
+      .then(function (response) {
+        log.info('bestBlock', response)
+        _this.start = response.data.height - 20
+        _this.end = response.data.height
+        _this.bestBlock = response.data
+        getTransactions(_this.start, _this.end, _this)
+      })
+      .catch(function (error) {
+        log.info(error, this)
+        log.info('this', this)
+      })
+  },
+  watch: {
+    // call again the method if the route changes
+    '$route': 'fetchStarterData'
+  },
+  filters: {
+    formatUnixDate: helpers.formatUnixDate
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.range-input {
+  width: 100px;
+}
+.input-label {
+  font-size: 12px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import ProofOfStake from '@/components/ProofOfStake'
+import Transactions from '@/components/Transactions'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Proof of Stake',
       component: ProofOfStake
+    },
+    {
+      path: '/transactions',
+      name: 'transactions',
+      component: Transactions
     }
   ]
 })
